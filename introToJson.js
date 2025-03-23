7b1d2d2b85c64262af06e954dfe27f763bc3a583
Sivabavan <bavan021015@gmail.com>
2025-03-23 06:22:19 +0530

    Create introToJson.js

---
+++ introToJson.js
@@ -0,0 +1,11 @@
+//JSON
+//{KEY: VALUE}
+let student = {regno:'2021/ICT/01',name:'james',age:'21',course:'IT',skills:['Java','JS','C++']}
+console.log(student)
+console.log(student.name)
+let students =[
+{regno:'2021/ICT/01',name:'james',age:'21',course:'IT'},
+{regno:'2021/ICT/02',name:'ravi',age:'22',course:'IT'},
+{regno:'2021/ICT/03',name:'gaya',age:'23',course:'IT'}
+]
+console.log(students)
