86c58b0c07d72b4127bdbf71364919e32eab05f0
Sivabavan <bavan021015@gmail.com>
2025-03-25 16:54:47 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,20 @@
+function PrintMsg()
+{	
+	console.log("Hello JS")
+}
+
+PrintMsg()
+
+function sum()
+{
+		return 8+6
+}
+
+console.log("Addition: " + sum())
+
+function sub(a,b)
+{
+		return a-b
+}
+
+console.log("Subtraction: "+sub(10,8))
