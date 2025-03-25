98c65069e3ca5d3515e761342ad2a18a891dc7bb
Sivabavan <bavan021015@gmail.com>
2025-03-25 17:12:28 +0530

    Create isPrime.js

---
+++ isPrime.js
@@ -0,0 +1,17 @@
+function isPrime(num) {
+  // Handle numbers less than 2
+  if (num <= 1) {
+    return false;
+  }
+  
+  // Check for factors from 2 to the square root of num
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false; // num is divisible by i, so it's not prime
+    }
+  }
+  
+  return true; // num is prime
+}
+
+
